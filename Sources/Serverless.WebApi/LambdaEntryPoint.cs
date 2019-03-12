582d226c2b792f27ca8e4032bc1d035700ad0113
Hannah Chan <hannah.chan@outlook.com>
2019-03-12 21:16:16 +1100

    Added AWS Lambda Entry Point.

---
+++ Sources/Serverless.WebApi/LambdaEntryPoint.cs
@@ -0,0 +1,10 @@
+namespace Serverless.WebApi
+{
+    using Amazon.Lambda.AspNetCoreServer;
+    using Microsoft.AspNetCore.Hosting;
+
+    public class LambdaEntryPoint : APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder) => builder.UseStartup<Startup>();
+    }
+}
